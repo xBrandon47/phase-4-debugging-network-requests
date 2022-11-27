f09f9119fab0e606c419be016605df08aae9c894
Brandon Moreno <brandonm99@me.com>
2022-11-26 16:26:00 -0800

    commit tree f530cb475f965444cecf55acfc6c041ee60ba20f

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
   def index
     movies = Movie.all
     render json: movies
